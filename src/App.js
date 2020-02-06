ec9910d159062c43bb8a8dcda70b0119a3c17360
IQ <santiphap.w@thoughtworks.com>
2020-02-06 13:55:53 +0700

    Add welcome message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+const WelcomeMessage = () => {
+  const  [error, setError] =  useState(false);
+  const  [response, setResponse] =  useState({});
+
+  async function fetchData() {
+    const res = await fetch("http://localhost:8080");
+    res.json()
+      .then(res => setResponse(res))
+      .catch(err => setError(err));
+  }
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  return (
+    <div className="WelcomeMessage">
+      <div class="jumbotron h5 my-3">
+        {response.response}
+      </div>
     </div>
   );
 }
