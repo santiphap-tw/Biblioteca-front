0257bc5fe9029636e48a65f5924e5646c715ab9b
IQ <santiphap.w@thoughtworks.com>
2020-02-12 09:08:45 +0700

    Separate user test from app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom';
 import { renderHook } from '@testing-library/react-hooks';
 import App from './App';
-import UserManagement from './User';
 
 it('should have welcome message, item management, and user management component', () => {
   const { result } = renderHook(() => App());
@@ -12,28 +11,4 @@ it('should have welcome message, item management, and user management component'
   expect(container.innerHTML).toMatch(/WelcomeMessage/);
   expect(container.innerHTML).toMatch(/ItemManagement/);
   expect(container.innerHTML).toMatch(/UserManagement/);
-});
-
-it('should show login form when no user', () => {
-  const { result } = renderHook(() => UserManagement({profile: null,update : null}));
-
-  const container = document.createElement('div');
-  ReactDOM.render(result.current, container);
-
-  expect(container.innerHTML).toMatch(/UserLogin/);
-});
-
-it('should show profile when have user', () => {
-  const profile = {
-    "id" : "111-1111",
-    "name" : "Name",
-    "email" : "Email",
-    "phone" : "Phone"
-  };
-  const { result } = renderHook(() => UserManagement({profile: profile,update : null}));
-
-  const container = document.createElement('div');
-  ReactDOM.render(result.current, container);
-
-  expect(container.innerHTML).toMatch(/UserProfile/);
 });
